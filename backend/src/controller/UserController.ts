c62b1497e0455f7ab72cb809fed3ac69b846b02b
vxfontes <nessa1vane@icloud.com>
2023-10-04 14:34:11 -0300

    feat: crud completo de usuario

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -1,50 +1,111 @@
 import { AppDataSource } from "../data-source"
 import { NextFunction, Request, Response } from "express"
 import { Usuario } from "../entity/User"
+import { PerfilEnum } from "../entity/Perfil"
 
 export class UserController {
 
     private userRepository = AppDataSource.getRepository(Usuario)
 
-    async all(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.find()
+
+    /**
+     * 
+     * @param request 
+     * @param response todos os usuarios do sistema
+     */
+    async all(request: Request, response: Response) {
+        try {
+            const usuarios = this.userRepository.find()
+            response.status(201).json({ status: 'success', message: 'Usuário encontrados com sucesso', usuarios: usuarios });
+        } catch (error) {
+            console.error('Erro ao obter usuários:', error);
+            response.status(500).json({ status: 'error', message: 'Erro ao obter usuários', error: error });
+        }
     }
 
-    async one(request: Request, response: Response, next: NextFunction) {
+
+    /**
+     * 
+     * @param request matricula
+     * @param response usuario que possui aquela matricula ou não
+     */
+    async one(request: Request, response: Response) {
         const matricula = request.params.matricula
 
-        const user = await this.userRepository.findOne({
-            where: { matricula }
-        })
+        try {
+            const user = await this.userRepository.findOne({
+                where: { matricula }
+            })
 
-        if (!user) {
-            return "Usuário não encontrado"
+            if (!user) {
+                response.status(400).json({ status: 'error', message: "Usuário não encontrado" });
+            }
+
+            response.status(201).json({ status: 'success', message: 'Usuário encontrado com sucesso', usuario: user });
+        } catch (error) {
+            console.error('Erro ao procurar o usuário:', error);
+            response.status(500).json({ status: 'error', message: 'Erro ao procurar o usuário', error: error });
         }
-        return user
     }
 
-    async save(request: Request, response: Response, next: NextFunction) {
-        const { nome, matricula, senha, verificado, perfil } = request.body;
 
-        const user = Object.assign(new Usuario(), {
-            nome, matricula, senha, verificado, perfil,
-        })
+    /**
+     * 
+     * @param request nome, matricula, senha, verificado, perfil
+     * @param response verifica se a matricula ja existe e retorna usuario criado ou não
+     */
+    async save(request: Request, response: Response) {
+        try {
+            const { nome, matricula, senha, verificado, perfil } = request.query;
 
-        return this.userRepository.save(user)
+            // Verificar se o perfil fornecido é válido
+            if (!Object.values(PerfilEnum).includes(perfil)) {
+                response.status(400).json({ status: 'error', message: 'Perfil inválido' });
+                return;
+            }
+
+            const existingUser = await this.userRepository.findOne({ where: { matricula } });
+
+            if (existingUser) {
+                response.status(400).json({ status: 'error', message: 'Matrícula já existe no banco de dados' });
+            } else {
+                const user = Object.assign(new Usuario(), {
+                    nome, matricula, senha, verificado, perfil,
+                });
+
+                await this.userRepository.save(user);
+
+                response.status(201).json({ status: 'success', message: 'Usuário criado com sucesso', usuario: user });
+            }
+        } catch (error) {
+            console.error('Erro ao criar o usuário:', error);
+            response.status(500).json({ status: 'error', message: 'Erro ao criar o usuário', error: error });
+        }
     }
 
-    async remove(request: Request, response: Response, next: NextFunction) {
+
+    /**
+     * 
+     * @param request matricula do usuario
+     * @param response se o usuario foi encontrado e deletado ou não
+     */
+    async remove(request: Request, response: Response) {
         const matricula = (request.params.matricula)
 
-        let userToRemove = await this.userRepository.findOneBy({ matricula })
+        try {
+            let userToRemove = await this.userRepository.findOneBy({ matricula })
 
-        if (!userToRemove) {
-            return "Usuário não encontrado"
+            if (!userToRemove) {
+                response.status(400).json({ status: 'error', message: "Usuário não encontrado" });
+            }
+
+            await this.userRepository.remove(userToRemove)
+
+            response.status(201).json({ status: 'success', message: `Usuário de matrícula: ${matricula} deletado com sucesso` });
+
+        } catch (error) {
+            console.error('Erro ao deletar o usuário:', error);
+            response.status(500).json({ status: 'error', message: 'Erro ao deletar o usuário', error: error });
         }
-
-        await this.userRepository.remove(userToRemove)
-
-        return "Usuário deletado"
     }
-
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,8 @@ import * as bodyParser from "body-parser";
 import { Request, Response } from "express";
 import { AppDataSource } from "./data-source";
 import { Routes } from "./routes"
+import { Usuario } from "./entity/User";
+import { PerfilEnum } from "./entity/Perfil";
 
 const port = 3003;
 const ip = '192.168.0.125';
@@ -32,14 +34,14 @@ AppDataSource.initialize().then(async () => {
     /* Definindo qual porta a aplicação vai rodar */
     app.listen(port, ip, () => console.log("O servidor está rodando na porta", port, "com ip", ip));
 
-    // // insert new users for test
+    // insert new users for test -> body
     // await AppDataSource.manager.save(
     //     AppDataSource.manager.create(Usuario, {
-    //         nome: "Teste nome",
-    //         matricula: "234234",
-    //         senha: "234dsf",
+    //         nome: "Teste nome 2",
+    //         matricula: "234234444",
+    //         senha: "234dsfdd",
     //         verificado: true,
-    //         perfil: PerfilEnum.Gerente,
+    //         perfil: PerfilEnum.Time,
     //     })
     // )
 }).catch(error => console.log(error))
