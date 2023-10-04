fe36e85b30a67548dd88b09b20f7b5c503ac83aa
Vanessa <63061509+vxfontes@users.noreply.github.com>
2023-10-03 22:28:43 -0300

    fix: consertando sintaxe

--- SQL/1criandoTabelas.sql
+++ SQL/1criandoTabelas.sql
@@ -2,30 +2,30 @@ CREATE SCHEMA ValidaQuero;
 
 CREATE TYPE ValidaQuero.perfil_enum AS ENUM ('Gestor', 'Gerente', 'Time');
 
-CREATE TABLE "ValidaQuero"."usuario" (
+CREATE TABLE ValidaQuero.usuario (
   "nome" VARCHAR(255) NOT NULL,
   "matricula" VARCHAR(30) PRIMARY KEY,
   "senha" VARCHAR(255) NOT NULL,
-  "perfil" perfil_enum NOT NULL,
+  "perfil" ValidaQuero.perfil_enum NOT NULL,
   "verificado" BOOLEAN NOT NULL
 );
 
 
 -- pendente, ativo, desativado
-CREATE TABLE "ValidaQuero"."status" (
+CREATE TABLE ValidaQuero.status (
     id SERIAL PRIMARY KEY,
     titulo VARCHAR(15)
 );
 
 -- csv, xls, xlsx
-CREATE TABLE "ValidaQuero"."formato" (
+CREATE TABLE ValidaQuero.formato (
     id SERIAL PRIMARY KEY,
     titulo VARCHAR(15)
 );
 
 -- data atual do sistema -> DATETIME DEFAULT GETDATE()
 -- EXEMPLO DE COMO VAI SER JSONB: [{ "nome": "campo1", "tipo": "inteiro", "nulo": true }, { "nome": "campo2", "tipo": "float", "nulo": false }] - precisa das aspas
-CREATE TABLE "ValidaQuero"."template" (
+CREATE TABLE ValidaQuero.template (
     id SERIAL PRIMARY KEY,
     titulo VARCHAR(30) NOT NULL,
     descricao TEXT NOT NULL,
@@ -35,12 +35,12 @@ CREATE TABLE "ValidaQuero"."template" (
     formato INTEGER NOT NULL,
     quantidadeCampos INTEGER NOT NULL,
     campos JSONB NOT NULL,
-    FOREIGN KEY (autor) REFERENCES usuario(matricula),
-    FOREIGN KEY (status) REFERENCES status(id),
-    FOREIGN KEY (formato) REFERENCES formato(id)
+    FOREIGN KEY (autor) REFERENCES ValidaQuero.usuario(matricula),
+    FOREIGN KEY (status) REFERENCES ValidaQuero.status(id),
+    FOREIGN KEY (formato) REFERENCES ValidaQuero.formato(id)
 );
 
-CREATE TABLE "ValidaQuero"."arquivo" (
+CREATE TABLE ValidaQuero.arquivo (
     id SERIAL PRIMARY KEY,
     titulo VARCHAR(30) NOT NULL,
     dataCriacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -49,6 +49,6 @@ CREATE TABLE "ValidaQuero"."arquivo" (
     template INTEGER NOT NULL,
     aprovado BOOLEAN NOT NULL,
     url VARCHAR,  -- pode ser nulo caso não esteja aprovado
-    FOREIGN KEY (autor) REFERENCES usuario(matricula),
-    FOREIGN KEY (template) REFERENCES template(id)
+    FOREIGN KEY (autor) REFERENCES ValidaQuero.usuario(matricula),
+    FOREIGN KEY (template) REFERENCES ValidaQuero.template(id)
 );
