44aa4d4b3ebb6d5539ff0d27da4f7dc45060db1e
Vanessa <63061509+vxfontes@users.noreply.github.com>
2023-10-04 10:37:42 -0300

    fix: removendo status

--- SQL/2tabelasFixas.sql
+++ SQL/2tabelasFixas.sql
@@ -1,5 +1,2 @@
 insert into "ValidaQuero"."formato" (titulo) values ('CSV'), ('XLSX'), ('XLS')
 select * from "ValidaQuero".formato 
-
-insert into "ValidaQuero".status (titulo) values ('Ativo'), ('Pendente'), ('Desativado')
-select * from "ValidaQuero".status s 
