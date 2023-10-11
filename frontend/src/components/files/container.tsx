2cc36385c46169b4b12c187a292e5fde8a6745d9
vxfontes <nessa1vane@icloud.com>
2023-10-11 01:19:26 -0300

    feat: adicionando arquivo real

--- frontend/src/components/files/container.tsx
+++ frontend/src/components/files/container.tsx
@@ -1,34 +1,42 @@
 import * as React from 'react';
-import { fakeArquivos } from "../../data/fakeArquivos";
 import { TemplateContainerGrid } from '../../styles/gridStyle';
 import { Pagination, Typography, Box } from '@mui/material';
 import FileButton from './fileButton';
+import { FileProps } from '../../logic/interfaces/file';
+import BoxLoading from '../muiComponents/boxLoading';
 
 interface Props {
-    itemsPerPage: number
+    itemsPerPage: number;
+    arquivos: FileProps[];
+    formato: string;
 }
 
-const FileContainer = ({ itemsPerPage }: Props) => {
-    const arquivo = fakeArquivos.filter((file) => file.aprovado === true);
+const FileContainer = ({ arquivos, itemsPerPage, formato }: Props) => {
     const getitemsPerPage = itemsPerPage;
     const [currentPage, setCurrentPage] = React.useState(1);
     const startIndex = (currentPage - 1) * getitemsPerPage;
     const endIndex = startIndex + getitemsPerPage;
 
-    const arquivosToDisplay = arquivo.slice(startIndex, endIndex);
+    const arquivosToDisplay = arquivos.slice(startIndex, endIndex);
 
     return (
         <TemplateContainerGrid item mt={2} xs={12} alignItems={'start'}>
             <Typography align='left' pl={3} py={2} variant="h6" color="initial">Lista de arquivos</Typography>
 
-            {arquivosToDisplay.map((file) => (
-                <FileButton key={file.id} file={file} />
-            ))}
+            {arquivos.length !== 0 ? (
+                <>
+                    {arquivosToDisplay.map((file) => (
+                        <FileButton key={file.id} file={file} formato={formato} />
+                    ))}
+                </>
+            ) : (
+                <BoxLoading message='Template ainda não possui arquivos' loading={false} />
+            )}
 
             <Box display="flex" justifyContent="flex-end" mt={2}>
                 <Pagination
                     sx={{ p: 1, pb: 2 }}
-                    count={Math.ceil(arquivo.length / getitemsPerPage)}
+                    count={Math.ceil(arquivos.length / getitemsPerPage)}
                     page={currentPage}
                     onChange={(_event, page) => setCurrentPage(page)}
                     color="secondary"
