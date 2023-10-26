c322c50eae65cd6c0e81af9deafa470a3475e3d0
vxfontes <nessa1vane@icloud.com>
2023-10-26 16:06:40 -0300

    fix: removendo firebase

--- frontend/src/logic/api/firebase.ts
+++
@@ -1,8 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
-import { firebaseConfig } from "./firebaseConfig";
-
-// /src/logic/api/firebaseConfig.ts -> criar arquivo e copiar as chaves do firebase
-
-const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app)
