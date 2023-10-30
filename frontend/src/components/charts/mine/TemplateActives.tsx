560c4fa63c585adf83c614e547de877c9fcd2dbc
vxfontes <nessa1vane@icloud.com>
2023-10-30 16:47:56 -0300

    feat: adicionando charts

--- frontend/src/components/charts/mine/TemplateActives.tsx
+++ frontend/src/components/charts/mine/TemplateActives.tsx
@@ -2,41 +2,9 @@ import { Box, Typography } from "@mui/material";
 import theme from "../../../theme";
 import { BoxSpanGray } from "../../muiComponents/boxes";
 import BarChart from "../BarChart";
+import { TemplateChartProps } from "./ArquivoRelatio";
 
-const TemplateCharts = () => {
-    const data = [
-        {
-            month: 'Janeiro',
-            ativos: 50,
-            inativos: 20,
-        },
-        {
-            month: 'Fevereiro',
-            ativos: 60,
-            inativos: 25,
-        },
-        {
-            month: 'Março',
-            ativos: 50,
-            inativos: 20,
-        },
-        {
-            month: 'Abril',
-            ativos: 60,
-            inativos: 25,
-        },
-        {
-            month: 'Maio',
-            ativos: 50,
-            inativos: 20,
-        },
-        {
-            month: 'Junho',
-            ativos: 60,
-            inativos: 25,
-        },
-    ];
-
+const TemplateCharts = ({ data }: {data: TemplateChartProps[]}) => {
     return (
         <Box sx={{
             backgroundColor: theme.palette.fundoComponente?.main,
@@ -46,9 +14,7 @@ const TemplateCharts = () => {
             pl: 3,
             pt: 2,
         }}>
-            <Typography variant="body1"><BoxSpanGray>Templates</BoxSpanGray></Typography>
-            <Typography variant="h5" color={theme.palette.primary.main} fontWeight='bold'>98</Typography>
-            <Typography variant="body1"><BoxSpanGray>Templates cadastrados</BoxSpanGray></Typography>
+            <Typography variant="body1"><BoxSpanGray>Templates adicionados por mês</BoxSpanGray></Typography>
             <BarChart data={data} />
         </Box>
     );
