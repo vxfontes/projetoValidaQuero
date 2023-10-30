3ff63daa28ab45d4c586b3c91a4cc461fece7dc5
vxfontes <nessa1vane@icloud.com>
2023-10-30 16:50:37 -0300

    feat: card de template

--- frontend/src/components/dashboard/CardData.tsx
+++ frontend/src/components/dashboard/CardData.tsx
@@ -1,12 +1,22 @@
-import { Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import theme from "../../theme";
 import icon from '../../assets/drawkit/perfil/circle.svg'
 import { BoxSpanGray } from "../muiComponents/boxes";
+import * as React from 'react';
 
 interface Props {
     type: 'template' | 'arquivo',
     value: number,
     subValue?: number
+}
+
+export interface TemplateBoxInfoProps {
+    value: number;
+    total: number;
+}
+export interface TemplateBoxProps extends TemplateBoxInfoProps {
+    isPending: boolean;
+    title: string;
 }
 
 const CardDataInfo = ({ type, value, subValue }: Props) => {
@@ -33,5 +43,35 @@ const CardDataInfo = ({ type, value, subValue }: Props) => {
         </Grid>
     );
 }
+
+export const CardDashBoardInfo: React.FC<TemplateBoxProps> = ({ title, value, total, isPending }) => {
+    return (
+        <Box
+            sx={{
+                backgroundColor: theme.palette.fundoComponente?.main,
+                boxShadow: '1px 2px 2px rgba(0, 0, 0, 0.25)',
+                borderRadius: '14px',
+                px: 2,
+                py: 1,
+                mt: 2
+            }}
+        >
+            <Grid item xs={12}>
+                <img src={icon} alt="Icone" />
+            </Grid>
+            <Grid item xs={12}>
+                <Typography variant="body1" color="initial">
+                    <BoxSpanGray>{title}</BoxSpanGray>
+                </Typography>
+                <Typography variant="h5" color="initial" display="flex" alignItems="end">
+                    {value}
+                    <Typography ml={1} variant="body1" color="initial">
+                        {isPending ? `/ ${total} pendentes` : `/ ${total} ativos`}
+                    </Typography>
+                </Typography>
+            </Grid>
+        </Box>
+    );
+};
 
 export default CardDataInfo;
