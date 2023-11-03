9c6650ee8cc33fe821b156dd7b3691f8a57aa294
vxfontes <nessa1vane@icloud.com>
2023-11-03 13:40:01 -0300

    fix: consertando campos com espaçamentos

--- frontend/src/logic/utils/transformaBoolean.ts
+++ frontend/src/logic/utils/transformaBoolean.ts
@@ -1,6 +1,7 @@
 export const transformaNulos = (campos: any) => {
     return campos.map((campo: any) => ({
         ...campo,
+        nome: campo.nome.trim(),
         nulo: campo.nulo === 'sim' ? true : false,
     }));
 };
