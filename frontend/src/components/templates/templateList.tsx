210517352687e2aeffbb7a7f048b3e837ff141ea
vxfontes <nessa1vane@icloud.com>
2023-11-03 13:05:36 -0300

    feat: filtro por nome

--- frontend/src/components/templates/templateList.tsx
+++ frontend/src/components/templates/templateList.tsx
@@ -1,19 +1,24 @@
 import * as React from 'react';
-import { Box, Divider, Grid, IconButton, MenuItem, Pagination, Select, Typography } from "@mui/material";
+import { Box, Divider, Grid, IconButton, InputAdornment, MenuItem, Pagination, Select, TextField, Typography } from "@mui/material";
 import { AiFillAppstore, AiOutlineMenu } from 'react-icons/ai'
 import GridContainers from "../muiComponents/gridContainers";
 import TemplateGrade from "./grade";
 import { Props } from './container';
+import { PiMagnifyingGlassBold } from 'react-icons/pi';
 
 export const GetTemplate = ({ templates, itemsPerPage, formatos, message }: Props) => {
 
     const [getitemsPerPage, setitemsPerPage] = React.useState(itemsPerPage);
+    const [searchValue, setSearchValue] = React.useState('');
     const [currentPage, setCurrentPage] = React.useState(1);
     const startIndex = (currentPage - 1) * getitemsPerPage;
     const endIndex = startIndex + getitemsPerPage;
 
     const [selectedFormat, setSelectedFormat] = React.useState('CSV');
-    const filteredTemplates = templates.filter((template) => {
+    const filteredByName = templates.filter((template) => {
+        return template.titulo.toLowerCase().includes(searchValue.toLowerCase());
+    });
+    const filteredTemplates = filteredByName.filter((template) => {
         return template.formato === selectedFormat;
     });
     const templatesToDisplay = filteredTemplates.slice(startIndex, endIndex);
@@ -27,13 +32,23 @@ export const GetTemplate = ({ templates, itemsPerPage, formatos, message }: Prop
     React.useMemo(() => {
         setCurrentPage(1);
     }, [selectedFormat]);
-    
+
     return (
         <>
             <Box pl={1} display='flex' justifyContent='space-between' alignItems='center'>
                 <Typography variant="h6" color="initial">Lista de templates</Typography>
                 <Box display='flex' alignItems='center'>
                     <Box>
+                        <TextField
+                            sx={{ mr: 1 }}
+                            variant="standard"
+                            placeholder='Pesquise por template...'
+                            value={searchValue}
+                            onChange={(e) => setSearchValue(e.target.value)}
+                            InputProps={{
+                                startAdornment: (<InputAdornment position="start"><PiMagnifyingGlassBold /></InputAdornment>),
+                            }}
+                        />
                         <Select value={selectedFormat} variant="standard" onChange={(e) => setSelectedFormat(e.target.value)}>
                             {formatos.map((format) => (
                                 <MenuItem key={format.id} value={format.titulo}>
