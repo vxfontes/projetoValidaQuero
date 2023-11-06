e3760b79bba001e678ba35d54b9b11e4c2c10296
vxfontes <nessa1vane@icloud.com>
2023-11-06 14:55:12 -0300

    fix: criação de module da database

--- backend-nest-js/src/app.module.ts
+++ backend-nest-js/src/app.module.ts
@@ -3,23 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './app/user/user.module';
 
 // isso é um decorator
 @Module({
-    imports: [
-        ConfigModule.forRoot(),
-        TypeOrmModule.forRoot({ // configurações de root
-            type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            database: 'postgres',
-            username: 'postgres',
-            password: 'postgres',
-            schema: 'ValidaQueroNestJs',
-            synchronize: true,
-            entities: [__dirname + '/**/*.entity{.js,.ts}'], // buscando todas as entidades ao inves de selecionar individualmente
-        })
-    ],
+    imports: [DatabaseModule, UserModule],
     controllers: [AppController], // são os arquivos que recebem rota
     providers: [AppService], // tudo que não é controller, é provider
 })
