33c7735a5bd26111a55c107b5bcb9897165a67c4
vxfontes <nessa1vane@icloud.com>
2023-11-06 19:55:23 -0300

    fix: removendo arquivos inuteis

--- backend-nest-js/src/app.module.ts
+++ backend-nest-js/src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './app/user/user.module';
+import { FormatoModule } from './formato/formato.module';
 
 // isso é um decorator
 @Module({
-    imports: [DatabaseModule, UserModule],
-    controllers: [AppController], // são os arquivos que recebem rota
-    providers: [AppService], // tudo que não é controller, é provider
+    imports: [DatabaseModule, UserModule, FormatoModule],
+    controllers: [], // são os arquivos que recebem rota
+    providers: [], // tudo que não é controller, é provider
 })
 export class AppModule { }
