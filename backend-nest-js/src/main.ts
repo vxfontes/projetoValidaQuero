33dc04b032a3f8bff09cba614945275c277bf6f6
vxfontes <nessa1vane@icloud.com>
2023-11-07 21:53:52 -0300

    feat: tentando resolver problema de cors

--- backend-nest-js/src/main.ts
+++ backend-nest-js/src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe())
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
