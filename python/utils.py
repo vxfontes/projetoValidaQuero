530ad32a99f18446f462d2ea51cdce2219215cbd
vxfontes <nessa1vane@icloud.com>
2023-11-12 19:44:07 -0300

    feat: transformando tipagem

--- python/utils.py
+++ python/utils.py
@@ -6,3 +6,15 @@ def formatoFile(filename):
 def load_file_data(file):
     arquivo = file.file.read()
     return io.BytesIO(arquivo)
+
+tipos_campos = [
+    {"pandas": "int64", "human": "inteiros"},
+    {"pandas": "float", "human": "decimais"},
+    {"pandas": "object", "human": "strings"},
+    {"pandas": "bool", "human": "booleanos"},
+    {"pandas": "datetime64", "human": "datas"}
+]
+
+def pandas_to_human(pandas_value):
+    tipo_campo = next((campo for campo in tipos_campos if campo["pandas"] == pandas_value), None)
+    return tipo_campo["human"] if tipo_campo else None
