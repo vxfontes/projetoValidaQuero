9e9dadd2dd5b52f1630c6d077741ec4b1ec3ee57
vxfontes <nessa1vane@icloud.com>
2023-12-29 12:18:18 -0300

    feat: adição de lista estatica de template

---
+++ lib/data/templates.dart
@@ -0,0 +1,142 @@
+const List<Object> templatesList = [
+  {
+    "title" : "Arquivos de desempenho",
+    "status" : "Ativo",
+    "colunas" : 4,
+    "formato" : "XLS",
+    "autor" : "Levi da Mata"
+  },
+  {
+    "title" : "Relatório de mercado",
+    "status" : "Desativado",
+    "colunas" : 2,
+    "formato" : "XLSX",
+    "autor" : "Bianca Cardoso"
+  },
+  {
+    "title" : "Relatório de orçamento",
+    "status" : "Desativado",
+    "colunas" : 3,
+    "formato" : "XLS",
+    "autor" : "Arthur da Cunha"
+  },
+  {
+    "title" : "Relatório de franquia",
+    "status" : "Desativado",
+    "colunas" : 5,
+    "formato" : "XLS",
+    "autor" : "Beatriz Rocha"
+  },
+  {
+    "title" : "Arquivos de estoque",
+    "status" : "Ativo",
+    "colunas" : 2,
+    "formato" : "XLS",
+    "autor" : "Sr. Luiz Fernando Rezende"
+  },
+  {
+    "title" : "Relatório de franquia",
+    "status" : "Ativo",
+    "colunas" : 5,
+    "formato" : "XLSX",
+    "autor" : "Sr. Juan Oliveira"
+  },
+  {
+    "title" : "Relatório de pagamento",
+    "status" : "Desativado",
+    "colunas" : 3,
+    "formato" : "CSV",
+    "autor" : "Davi Lucca Pires"
+  },
+  {
+    "title" : "Arquivos de publicidade",
+    "status" : "Ativo",
+    "colunas" : 5,
+    "formato" : "CSV",
+    "autor" : "Yago Ramos"
+  },
+  {
+    "title" : "Arquivos de negócios",
+    "status" : "Ativo",
+    "colunas" : 4,
+    "formato" : "XLS",
+    "autor" : "Srta. Sabrina Viana"
+  },
+  {
+    "title" : "Arquivos de comércio",
+    "status" : "Desativado",
+    "colunas" : 4,
+    "formato" : "XLSX",
+    "autor" : "Sr. Juan Oliveira"
+  },
+  {
+    "title" : "Relatório de demografia",
+    "status" : "Ativo",
+    "colunas" : 4,
+    "formato" : "CSV",
+    "autor" : "Maria Fernanda da Cruz"
+  },
+  {
+    "title" : "Template de comércio",
+    "status" : "Ativo",
+    "colunas" : 5,
+    "formato" : "XLS",
+    "autor" : "Arthur da Cunha"
+  },
+  {
+    "title" : "Relatório de mercado",
+    "status" : "Desativado",
+    "colunas" : 3,
+    "formato" : "CSV",
+    "autor" : "Bruna Pereira"
+  },
+  {
+    "title" : "Template de preço",
+    "status" : "Ativo",
+    "colunas" : 2,
+    "formato" : "CSV",
+    "autor" : "Arthur da Cunha"
+  },
+  {
+    "title" : "Relatório de produto",
+    "status" : "Desativado",
+    "colunas" : 4,
+    "formato" : "XLSX",
+    "autor" : "Beatriz Rocha"
+  },
+  {
+    "title" : "Template de mercado",
+    "status" : "Desativado",
+    "colunas" : 3,
+    "formato" : "CSV",
+    "autor" : "Dr. Luiz Henrique Barbosa"
+  },
+  {
+    "title" : "Template de produção",
+    "status" : "Desativado",
+    "colunas" : 2,
+    "formato" : "XLS",
+    "autor" : "Dr. Luiz Henrique Barbosa"
+  },
+  {
+    "title" : "Relatório de produto",
+    "status" : "Desativado",
+    "colunas" : 4,
+    "formato" : "XLS",
+    "autor" : "Anthony Costela"
+  },
+  {
+    "title" : "Relatório de visibilidade",
+    "status" : "Ativo",
+    "colunas" : 3,
+    "formato" : "XLS",
+    "autor" : "Levi da Mata"
+  },
+  {
+    "title" : "Template de pesquisa",
+    "status" : "Desativado",
+    "colunas" : 3,
+    "formato" : "CSV",
+    "autor" : "Sr. Luiz Fernando Rezende"
+  },
+];
