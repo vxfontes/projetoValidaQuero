46e3d0c929911e8373ae39d9fc69820011b7a939
vxfontes <nessa1vane@icloud.com>
2023-12-31 19:54:26 -0300

    feat: formatador de data

---
+++ lib/components/texts/data_help.dart
@@ -0,0 +1,7 @@
+String DataHelp(DateTime data) {
+  String dia = data.day.toString().padLeft(2, '0');
+  String mes = data.month.toString().padLeft(2, '0');
+  String ano = data.year.toString();
+
+  return '$dia/$mes/$ano';
+}
