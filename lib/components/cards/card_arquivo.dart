2743871dbca3357a22b07dbabe4ac71d51a110c2
vxfontes <nessa1vane@icloud.com>
2023-12-31 20:48:41 -0300

    feat: modal para arquivo

--- lib/components/cards/card_arquivo.dart
+++ lib/components/cards/card_arquivo.dart
@@ -1,42 +1,61 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:validaquero/components/buttons/ColorButton.dart';
+import 'package:validaquero/components/modals/modal_arquivo.dart';
 import 'package:validaquero/components/texts/data_help.dart';
 import 'package:validaquero/components/texts/texts.dart';
+import 'package:validaquero/models/arquivo_model.dart';
+import 'package:validaquero/themes/app_colors.dart';
 
 class CardArquivo extends StatelessWidget {
-  const CardArquivo({super.key, required this.title, required this.template, required this.data});
-  final String title, template, data;
+  const CardArquivo({super.key, required this.arquivo});
+
+  final Arquivo arquivo;
 
   @override
   Widget build(BuildContext context) {
-    final DateTime dataBruta = DateTime.parse(data);
+    final DateTime dataBruta = DateTime.parse(arquivo.data);
 
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            TextTitleBold(title: title, arquivo: true,),
-            Text(
-              DataHelp(dataBruta),
-              style: const TextStyle(fontSize: 17),
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            Text(
-              template,
-              style: const TextStyle(fontSize: 17, color: Colors.black54),
-            )
-          ],
-        ),
-        const Padding(
-          padding: EdgeInsets.symmetric(vertical: 3.0),
-          child: Divider(
-            color: Colors.black12,
+    return GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (BuildContext context) {
+            return ModalArquivo(arquivo: arquivo);
+          },
+        );
+      },
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TextTitleBold(
+                title: arquivo.title,
+                arquivo: true,
+              ),
+              Text(
+                DataHelp(dataBruta),
+                style: const TextStyle(fontSize: 17),
+              ),
+            ],
           ),
-        )
-      ],
+          Row(
+            children: [
+              Text(
+                arquivo.template,
+                style: const TextStyle(fontSize: 17, color: Colors.black54),
+              )
+            ],
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 3.0),
+            child: Divider(
+              color: Colors.black12,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/components/modals/modal_arquivo.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:validaquero/components/texts/data_help.dart';
+import 'package:validaquero/components/texts/texts.dart';
+import 'package:validaquero/models/arquivo_model.dart';
+import 'package:validaquero/themes/app_colors.dart';
+
+class ModalArquivo extends StatelessWidget {
+  const ModalArquivo({super.key, required this.arquivo});
+
+  final Arquivo arquivo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 290,
+      padding: const EdgeInsets.all(25),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  arquivo.title,
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(children: [
+            TextNumberLegend(label: 'Template: ', value: arquivo.template)
+          ]),
+          Row(children: [
+            TextNumberLegend(label: 'Formato: ', value: arquivo.formato)
+          ]),
+          Row(children: [
+            TextNumberLegend(
+                label: 'Data de criação: ',
+                value: DataHelp(DateTime.parse(arquivo.data)))
+          ]),
+          Row(children: [
+            TextNumberLegend(label: 'Autor: ', value: arquivo.autor)
+          ]),
+          Row(children: [
+            TextNumberLegend(
+                label: 'Quantidade de linhas: ',
+                value: arquivo.linhas.toString())
+          ]),
+          Row(children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    backgroundColor: ThemeColors.primaryColor,
+                  ),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.download,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: 8),
+                      Text(
+                        'Download',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 1.2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ])
+        ],
+      ),
+    );
+  }
+}
