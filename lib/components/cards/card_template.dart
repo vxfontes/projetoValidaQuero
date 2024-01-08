5e5c51fdcc1b759ecf337fee7da146bedf77b507
vxfontes <nessa1vane@icloud.com>
2024-01-08 15:55:24 -0300

    feat: tela de template

--- lib/components/cards/card_template.dart
+++ lib/components/cards/card_template.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:validaquero/components/texts/chip.dart';
 import 'package:validaquero/components/texts/texts.dart';
 import 'package:validaquero/models/template_model.dart';
+import 'package:validaquero/screens/template_details.dart';
 import 'package:validaquero/themes/app_colors.dart';
 
 class CardTemplate extends StatelessWidget {
@@ -13,49 +14,57 @@ class CardTemplate extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 18.0),
-      child: Container(
-        height: 162,
-        width: 300,
-        padding: const EdgeInsets.all(15),
-        decoration: BoxDecoration(
-          color: ThemeColors.fundoComponenteColor,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                TextTitleBold(title: template.titulo, arquivo: false),
-                ChipPink(label: template.formato),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 5, 0, 2),
-              child: Row(
-                children: [
-                  TextNumberLegend(
-                      label: 'Número de colunas: ', value: template.quantidadeCampos.toString())
-                ],
-              ),
-            ),
-            Row(
-              children: [TextNumberLegend(label: 'Autor: ', value: template.usuario.nome)],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: Row(
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => TemplateDetails(template: template)),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 18.0),
+        child: Container(
+          height: 162,
+          width: 300,
+          padding: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            color: ThemeColors.fundoComponenteColor,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  ChipAtivoDesativado(
-                      label: template.status, ativo: template.status == 'Ativo' ? true : false),
+                  TextTitleBold(title: template.titulo, arquivo: false),
+                  ChipPink(label: template.formato),
                 ],
               ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 5, 0, 2),
+                child: Row(
+                  children: [
+                    TextNumberLegend(
+                        label: 'Número de colunas: ', value: template.quantidadeCampos.toString())
+                  ],
+                ),
+              ),
+              Row(
+                children: [TextNumberLegend(label: 'Autor: ', value: template.usuario.nome)],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ChipAtivoDesativado(
+                        label: template.status, ativo: template.status == 'Ativo' ? true : false),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/screens/template_details.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:validaquero/components/texts/chip.dart';
+import 'package:validaquero/components/texts/tables.dart';
+import 'package:validaquero/components/texts/texts.dart';
+import 'package:validaquero/models/template_model.dart';
+import 'package:validaquero/themes/app_colors.dart';
+import 'package:validaquero/utils/data_help.dart';
+
+class TemplateDetails extends StatelessWidget {
+  const TemplateDetails({super.key, required this.template});
+
+  final Template template;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(template.titulo),
+        backgroundColor: ThemeColors.fundoComponenteColor,
+      ),
+      backgroundColor: ThemeColors.fundoPrincipalColor,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Container(
+            padding: const EdgeInsets.all(14.0),
+            decoration: BoxDecoration(
+              color: ThemeColors.fundoComponenteColor,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      TextTitleBig(title: template.titulo),
+                      ChipAtivoDesativado(
+                        label: template.status,
+                        ativo: template.status == 'Ativo' ? true : false,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextNumberLegend(
+                    label: 'Descrição ',
+                    value: template.descricao,
+                  ),
+                ),
+                TabelaCampos(campos: template.campos),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Row(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(right: 8.0),
+                            child: Icon(Icons.account_circle_outlined),
+                          ),
+                          Text(template.usuario.nome),
+                        ],
+                      ),
+                      Column(children: [
+                        const Text('Data de criação'),
+                        Text(dataHelp(DateTime.parse(template.dataCriacao))),
+                      ])
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ChipPink(label: template.formato),
+                    const Icon(
+                      Icons.download_rounded,
+                      color: ThemeColors.primaryColor,
+                    ),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: ThemeColors.primaryColor,
+                        ),
+                        child: const Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.cloud_upload,
+                              color: Colors.white,
+                            ),
+                            SizedBox(width: 8),
+                            Text(
+                              'Upload de arquivo',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
