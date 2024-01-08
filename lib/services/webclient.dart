6c6351604b19e236d1e0bfd66e536940521bc001
vxfontes <nessa1vane@icloud.com>
2024-01-08 11:08:50 -0300

    fix: colocando ip

--- lib/services/webclient.dart
+++ lib/services/webclient.dart
@@ -3,7 +3,7 @@ import 'package:http_interceptor/http/http.dart';
 import 'http_interceptor.dart';
 
 class WebClient {
-  static const String url = "http://localhost:3000/";
+  static const String url = "http://192.168.0.106:3000/";
 
   http.Client client = InterceptedClient.build(
     interceptors: [LoggingInterceptor()],
