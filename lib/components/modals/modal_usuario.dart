e93d7c1994ed1964292938e5ab389020d7567b6e
vxfontes <nessa1vane@icloud.com>
2024-01-08 23:48:20 -0300

    fix: removendo algumas rows

--- lib/components/modals/modal_usuario.dart
+++ lib/components/modals/modal_usuario.dart
@@ -15,78 +15,61 @@ class ModalUsuario extends StatelessWidget {
       padding: const EdgeInsets.all(25),
       child: Column(
         children: [
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: Text(
-                  usuario.nome,
-                  style: const TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Text(
+              usuario.nome,
+              style: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w500,
               ),
-            ],
+            ),
           ),
-          Row(children: [
-            TextNumberLegend(label: 'Matrícula: ', value: usuario.matricula)
-          ]),
-          Row(children: [
-            TextNumberLegend(label: 'Perfil: ', value: usuario.perfil)
-          ]),
-          Row(children: [
-            TextNumberLegend(
-                label: 'Verificado: ',
-                value: usuario.verificado == true ? "Sim" : "Não")
-          ]),
-          Row(children: [
-            TextNumberLegend(
-                label: 'Templates cadastrados: ',
-                value:
-                    "${usuario.template.ativo}/${usuario.template.ativo + usuario.template.desativado} ativos")
-          ]),
-          Row(children: [
-            TextNumberLegend(
-                label: 'Arquivos submetidos: ',
-                value:
-                "${usuario.arquivo.aprovados} arquivos")
-          ]),
-          Row(children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 15),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
+          TextNumberLegend(label: 'Matrícula: ', value: usuario.matricula),
+          TextNumberLegend(label: 'Perfil: ', value: usuario.perfil),
+          TextNumberLegend(
+              label: 'Verificado: ',
+              value: usuario.verificado == true ? "Sim" : "Não"),
+          TextNumberLegend(
+              label: 'Templates cadastrados: ',
+              value:
+                  "${usuario.template.ativo}/${usuario.template.ativo + usuario.template.desativado} ativos"),
+          TextNumberLegend(
+              label: 'Arquivos submetidos: ',
+              value: "${usuario.arquivo.aprovados} arquivos"),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  backgroundColor: ThemeColors.primaryColor,
+                ),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.restore_from_trash,
+                      color: Colors.white,
                     ),
-                    backgroundColor: ThemeColors.primaryColor,
-                  ),
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.restore_from_trash,
+                    SizedBox(width: 8),
+                    Text(
+                      'Apagar',
+                      style: TextStyle(
                         color: Colors.white,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 1.2,
                       ),
-                      SizedBox(width: 8),
-                      Text(
-                        'Apagar',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Roboto',
-                          fontWeight: FontWeight.w400,
-                          letterSpacing: 1.2,
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            )
-          ])
+            ),
+          )
         ],
       ),
     );
