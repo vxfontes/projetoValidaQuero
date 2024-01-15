346e2bb6f216666300adde122e4454871d7bb18b
vxfontes <nessa1vane@icloud.com>
2024-01-15 16:42:20 -0300

    feat: criando pasta mobile

--- lib/data/perfil.dart
+++
@@ -1,5 +0,0 @@
-final List<String> perfil = [
-  'Gestor',
-  'Gerente',
-  'Time',
-];
--- lib/data/tipos.dart
+++
@@ -1,7 +0,0 @@
-final List<Map<String, String>> tipos = [
-  { "pandas": "int64", "human": "Inteiro" },
-  { "pandas": "float", "human": "Decimal" },
-  { "pandas": "object", "human": "String" },
-  { "pandas": "bool", "human": "Boolean" },
-  { "pandas": "datetime64", "human": "Data" }
-];
