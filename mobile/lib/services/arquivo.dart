5717866537639e0f61f1dfe0baaeec576706335c
vxfontes <nessa1vane@icloud.com>
2024-01-16 12:09:27 -0300

    fix: consertando forma de envio

--- mobile/lib/services/arquivo.dart
+++ mobile/lib/services/arquivo.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:file_picker/file_picker.dart';
 import 'package:validaquero/services/webclient.dart';
 import 'package:http/http.dart' as http;
 
@@ -17,7 +18,7 @@ class ArquivoService {
   }
 
   Future<Map<String, dynamic>> uploadFile(
-    file,
+    FilePickerResult file,
     String titulo,
     String usuario,
     bool publico,
@@ -27,23 +28,34 @@ class ArquivoService {
     campos,
   ) async {
     print('ENTREI NO PYTHON');
-    var formDataMap = Map<String, dynamic>();
-    formDataMap['file'] = file;
-    formDataMap['titulo'] = titulo;
-    formDataMap['usuario'] = usuario;
-    formDataMap['publico'] = publico.toString();
-    formDataMap['template'] = template.toString();
-    formDataMap['verify'] = 'true';
-    formDataMap['repositorio'] = repositorio;
-    formDataMap['formato'] = formato;
 
-    http.Response response = await client.post(
-      Uri.parse("${url}file/upload/"),
-      body: formDataMap,
+    var formData = http.MultipartRequest(
+      'POST',
+      Uri.parse("${python}file/upload/"),
     );
 
-    print(response.body);
+    formData.files.add(http.MultipartFile.fromBytes(
+      'file',
+      file.files.single.bytes!,
+      filename: file.files.single.name,
+    ));
 
-    return json.decode(response.body);
+    formData.fields['titulo'] = titulo;
+    formData.fields['usuario'] = usuario;
+    formData.fields['publico'] = publico.toString();
+    formData.fields['template'] = template.toString();
+    formData.fields['verify'] = 'true';
+    formData.fields['repositorio'] = repositorio;
+    formData.fields['formato'] = formato;
+
+    try {
+      http.Response response = await http.Response.fromStream(await formData.send());
+
+      print('Resposta do servidor: ${response.body}');
+      return json.decode(response.body);
+    } catch (e) {
+      print('Erro durante o envio do arquivo: $e');
+      return json.decode(e.toString());
+    }
   }
 }
